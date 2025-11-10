543245cbcc6356271003e0198703b73fab90afbe
vitalien-bytes <contact@digitaltelecomnetwork.fr>
2025-11-10 23:49:47 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,13 @@
-exite
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# URL de la base PostgreSQL (vient des variables Render)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Connexion SQLAlchemy
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
