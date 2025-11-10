c0e824ebfdcd26b324690eca77c61578e36503b0
vitalien-bytes <contact@digitaltelecomnetwork.fr>
2025-11-10 23:50:20 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,24 @@
-from
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Board(Base):
+    __tablename__ = "boards"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    columns = relationship("Column", back_populates="board")
+
+class Column(Base):
+    __tablename__ = "columns"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    board_id = Column(Integer, ForeignKey("boards.id"))
+    board = relationship("Board", back_populates="columns")
+    cards = relationship("Card", back_populates="column")
+
+class Card(Base):
+    __tablename__ = "cards"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    column_id = Column(Integer, ForeignKey("columns.id"))
+    column = relationship("Column", back_populates="cards")
